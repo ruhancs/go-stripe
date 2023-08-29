d65c60f64da4d82b8ef535ecf5de76e68de5c3a4
ruhancs <ruhan_cs@hotmail.com>
2023-08-29 19:39:14 -0300

    fix: erro digigitacao handler

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -521,7 +521,7 @@ func (app *application) ResetPassword(w http.ResponseWriter, r * http.Request) {
 	app.writeJSON(w, http.StatusCreated, resp)
 }
 
-func(app *application) AllSales(w http.ResponseWriter, *r *http.Request) {
+func(app *application) AllSales(w http.ResponseWriter, r * http.Request) {
 	allSales,err := app.DB.GetAllOrders()
 	if err != nil {
 		app.badRequest(w, r, err)
