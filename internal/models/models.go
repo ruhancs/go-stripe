677d60800a721062a15bc5fc3d14de802bbf00bd
ruhancs <ruhan_cs@hotmail.com>
2023-09-02 20:03:40 -0300

    feat: adicionado paginacao nos dados

--- internal/models/models.go
+++ internal/models/models.go
@@ -511,6 +511,92 @@ func (m *DbModel) GetAllSubscriptions() ([]*Order, error) {
 	return orders, nil
 }
 
+func (m *DbModel) GetAllSubscriptionsPaginated(page, pageSize int) ([]*Order,int, int, error) {
+	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	offset := (page - 1) * pageSize //inicio e fim dos dados inserido na busca do db
+
+	var orders []*Order
+
+	query := `
+	select
+		o.id, o.widget_id, o.transaction_id, o.customer_id, 
+		o.status_id, o.quantity, o.amount, o.created_at,
+		o.updated_at, w.id, w.name, t.id, t.amount, t.currency,
+		t.last_four, t.expiry_month, t.expiry_year, t.payment_intent,
+		t.bank_return_code, c.id, c.first_name, c.last_name, c.email	
+	
+	from
+		orders o
+		left join widgets w on (o.widget_id = w.id)
+		left join transactions t on (o.transaction_id = t.id)
+		left join customers c on (o.customer_id = c.id)
+	where
+		w.is_recurring = 1
+	order by
+		o.created_at desc
+	limit ? offset ?
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query, pageSize, offset)
+	if err != nil {
+		return nil,0,0,err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var o Order
+		err = rows.Scan(
+			&o.ID,
+			&o.WidgetID,
+			&o.TransactionID,
+			&o.CustomerID,
+			&o.StatusID,
+			&o.Quantity,
+			&o.Amount,
+			&o.CreatedAt,
+			&o.UpdatedAt,
+			&o.Widget.ID,
+			&o.Widget.Name,
+			&o.Transaction.ID,
+			&o.Transaction.Amount,
+			&o.Transaction.Currency,
+			&o.Transaction.LastFour,
+			&o.Transaction.ExpiryMonth,
+			&o.Transaction.ExpiryYear,
+			&o.Transaction.PaymentIntent,
+			&o.Transaction.BankReturnCode,
+			&o.Customer.ID,
+			&o.Customer.FirstName,
+			&o.Customer.LastName,
+			&o.Customer.Email,
+		)
+		if err != nil {
+			return nil,0,0,err
+		}
+
+		orders = append(orders, &o)
+	}
+
+	//todas as orders das widgets
+	query = `
+		select count(o.id)
+		from orders o
+		left join widgets w on (o.widget_id = w.id)
+		where w.is_recurring = 1
+	`
+	var totalRecords int
+	countRow := m.DB.QueryRowContext(ctx,query)
+	err = countRow.Scan(&totalRecords)
+	if err != nil {
+		return nil,0,0,err
+	}
+	lastPage := totalRecords / pageSize
+
+	return orders,lastPage, totalRecords, nil
+}
+
 func (m *DbModel) GetOrderByID(orderID int) (Order, error) {
 	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
 	defer cancel()
@@ -576,6 +662,112 @@ func (m *DbModel) UpdateOrderStatus(id, statusID int) error {
 
 	_, err := m.DB.ExecContext(ctx,stmt, statusID, id)
 	if err !=  nil {
+		return err
+	}
+	return nil
+}
+
+func (m *DbModel) GetAllUsers() ([]*User, error) {
+	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	var users []*User
+
+	query := `
+		select id, last_name, first_name, email, created_at, updated_at
+		from users
+		order by last_name, first_name
+	`
+
+	rows,err := m.DB.QueryContext(ctx,query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var u User
+		err := rows.Scan(
+			&u.ID,
+			&u.LastName,
+			&u.FirstName,
+			&u.Email,
+			&u.CreatedAt,
+			&u.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, &u)
+	}
+	return users, nil
+}
+
+func (m *DbModel) GetUser(id int) (User, error) {
+	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	var user User
+
+	query := `
+		select id, last_name, first_name, email, created_at, updated_at
+		from users
+		where id=?
+	`
+
+	row := m.DB.QueryRowContext(ctx,query,id)
+
+	err := row.Scan(
+		&user.ID,
+		&user.LastName,
+		&user.FirstName,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (m *DbModel) EditUser(u User) error {
+	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	stmt := `
+		update users set first_name=? last_name=?, email=?, updated_at=?
+		where id=?
+	`
+	_,err := m.DB.ExecContext(ctx,stmt,u.FirstName,u.LastName,u.Email,time.Now(),u.ID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *DbModel) AddtUser(u User, hash string) error {
+	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	stmt := `
+		insert into users (first_name,last_name,email,password,created_at,updated_at)
+		values(?,?,?,?,?,?)
+	`
+	_,err := m.DB.ExecContext(ctx,stmt,u.FirstName,u.LastName,u.Email,hash,time.Now(),time.Now())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *DbModel) DeleteUser(id int) error {
+	ctx,cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+
+	stmt := `delete from users where id=?`
+	_,err := m.DB.ExecContext(ctx,stmt,id)
+	if err != nil {
 		return err
 	}
 	return nil
